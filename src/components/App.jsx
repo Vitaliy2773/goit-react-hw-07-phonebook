ce1914211e7a50c0b80aca36e9a6328e17a617c3
Vitaliy2773 <vitalii64773@gmail.com>
2023-11-24 15:15:26 -1100

    hw7

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,57 @@
-export const App = () => {
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import {
+  fetchContacts,
+  addContact,
+  deleteContact,
+  setFilter,
+} from './contactsSlice/contactsSlice.jsx';
+import ContactForm from './contactForm/ContactForm.jsx';
+import Filter from './filter/Filter.jsx';
+import ContactList from './contactList/ContactList.jsx';
+import css from './Contacts.module.css';
+
+export default function App() {
+  const dispatch = useDispatch();
+  const {
+    items: contacts,
+    isLoading,
+    error,
+    filter,
+  } = useSelector(state => state.contacts);
+
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
+
+  const handleSubmit = (name, number) => {
+    dispatch(addContact({ name, number }));
+  };
+
+  const handleDeleteContact = contactId => {
+    dispatch(deleteContact(contactId));
+  };
+
+  const changeFilter = event => {
+    dispatch(setFilter(event.target.value));
+  };
+
+  const filteredContacts = contacts.filter(contact =>
+    contact.name.toLowerCase().includes(filter.toLowerCase())
+  );
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div className={css.container}>
+      <h1 className={css.title}>Phonebook</h1>
+      <ContactForm onSubmit={handleSubmit} />
+      <h2 className={css.title}>Contacts</h2>
+      <Filter value={filter} onChange={changeFilter} />
+      {isLoading && <p>Loading...</p>}
+      {error && <p>Error: {error}</p>}
+      <ContactList
+        contacts={filteredContacts}
+        onDeleteItem={handleDeleteContact}
+      />
     </div>
   );
-};
+}
